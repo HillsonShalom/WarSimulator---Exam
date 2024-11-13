98c6041123c0141e7de65db7a7e3218a1a68a3eb
Hillson <hillsonshalom@gmail.com>
2024-11-13 13:38:15 +0200

    demands token and populate on it

--- Server/src/middlewares/verifyToken.guard.ts
+++ Server/src/middlewares/verifyToken.guard.ts
@@ -1,9 +1,11 @@
-//  /// <reference path="../types/extensions/appRequest.d.ts" />
+/// <reference path="../types/extensions/appRequest.d.ts" />
 
 import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken';
 import AppResError from "../types/extensions/AppResError";
-import TokenDto from "../types/DTOs/token.dto";
+import IToken from "../types/models/token";
+import User from "../types/schemas/userSchema";
+import Organization from "../types/schemas/organizationSchema";
 
 export const verifyToken = async (
   req: Request,
@@ -15,7 +17,13 @@ export const verifyToken = async (
     const token = req.header("Authorization")
     if (!token) throw new AppResError(401, "Login first!");
 
-    req.token = jwt.verify(token, process.env.SECRET_KEY!) as TokenDto;
+    req.token = jwt.verify(token, process.env.SECRET_KEY!) as IToken;
+    if (!req.token) throw new AppResError(401, "Wrong token!");
+
+    const user = await User.findById(req.token.id).populate("organization").exec();
+    if (!user) throw new AppResError(404, "User not found!");
+    req.token.id = user;
+    
     next();
   } catch (err) {
     const error = err as AppResError;
