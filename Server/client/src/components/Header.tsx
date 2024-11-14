e9ea01776834daced7bb8b196ea3366e7ecdd77f
Hillson <hillsonshalom@gmail.com>
2024-11-14 18:18:45 +0200

    some css

--- Server/client/src/components/Header.tsx
+++ Server/client/src/components/Header.tsx
@@ -3,8 +3,10 @@ import { NavLink } from "react-router-dom"
 const Header = () => {
   return (
     <div className="header">
+        <NavLink to={'/account'} >Home</NavLink>
         <NavLink to={'/attack'} >Attack</NavLink>
         <NavLink to={'/defend'} >Defend</NavLink>
+        <NavLink to={'/store'} >Store</NavLink>
         <NavLink to={'/account/login'} >Login</NavLink>
         <NavLink to={'/account/register'} >Register</NavLink>
     </div>
